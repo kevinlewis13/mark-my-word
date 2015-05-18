46963117aefa7bfd2d265b31d0965b1e12b5dd20
Stefan Redfield <redfield.stefan@gmail.com>
2015-05-18 12:49:46 -0700

    code cleanup

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -8,6 +8,7 @@ module.exports = function(router) {
 
   router.get('/login', function(req, res) {
     res.json({msg: 'login hit'});
+
   });
 
   // To create a new user send an object with username, password and email
@@ -20,6 +21,7 @@ module.exports = function(router) {
     }
     newUser.username = req.body.username;
     newUser.basic.email = req.body.email;
+    newUser.generateTokenId();
     newUser.generateHash(req.body.password, function(err, hash) {
       if (err) {
         return res.status(500).json({msg: 'Account could not be created'});
