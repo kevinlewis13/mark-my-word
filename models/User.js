a195f08c1c5635a84ba96cd3819b97036708360d
Stefan Redfield <redfield.stefan@gmail.com>
2015-05-18 11:22:47 -0700

    code cleanup

--- models/User.js
+++ models/User.js
@@ -4,10 +4,22 @@ var mongoose = require('mongoose');
 var bcrypt = require('bcrypt-nodejs');
 
 var userSchema = new mongoose.Schema({
-	username: {type: String, required:true, unique:true, trim:true},
+	username: {
+		type: String, 
+		required:true, 
+		unique:true, 
+		trim:true
+	},
 	basic:{
-		email: {type: String, required:true, unique:true},
-		password:{type: String, required:true}
+		email: {
+			type: String,
+			required:true,
+			unique:true
+		},
+		password:{
+			type: String, 
+			required:true
+		}
 	},
 	tokenId: Number,
 	city: String,
