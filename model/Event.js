236f590cb7d53dc9a3a346e7fcb400b62ae572ae
Stefan Redfield <redfield.stefan@gmail.com>
2015-05-18 09:34:02 -0700

    start defining schemas

--- model/Event.js
+++ model/Event.js
@@ -1,2 +1,24 @@
+'use strict';
 
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+var uuid = require('uuid');
+var eat = require('eat');
+
+
+var eventSchema = new mongoose.Schema({
+	name: String,
+	location: String,
+	startDate: Number,
+	expDate: Number
+	bets: {
+		win: boolean,
+		lose: boolean,
+		random; boolean,
+		random2; boolean,
+		random3; boolean
+	}
+	users: [],
+	id: String
+});
 
--- model/User.js
+++ model/User.js
@@ -1,3 +1,25 @@
+'use strict';
+
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt-nodejs');
+var uuid = require('uuid');
+var eat = require('eat');
+
+
+var eventSchema = new mongoose.Schema({
+	username: String,
+	basic:{
+		email: {type: String, unique: true, required: true},
+		password:{type: String, unique:true, required: true}
+	}
+	location: String,
+	wins: Number,
+	losses: Number,
+	bets: Number,
+	record: this.wins/this.bets,
+	tokenId: String,
+	events: []
+});
 
 
 
