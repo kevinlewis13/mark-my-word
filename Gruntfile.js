c6421538d065c46681afb9dbe6768b34efd2a13e
Stefan Redfield <redfield.stefan@gmail.com>
2015-05-18 10:11:48 -0700

    add Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt) {
+
+	grunt.loadNpmTask('grunt-contrib-jshint');
+	grunt.loadNpmTask('grunt-simple-mocha');
+	grunt.loadNpmTask('grunt-nodemon');
+
+
+	var srcFiles = ['Gruntfile.js', './models/**/*.js', './test/**/*test.js'];
+
+	grunt.intiConfig({
+
+		jshint:{
+			dev:{
+				src: srcFiles
+			},
+
+			options:{
+				jshint:true
+			}
+		},
+
+		simplemocha:{
+			dev:{
+				src:['./test/**/*test.js']
+			}
+		},
+
+		nodemond:{
+			dev:{
+				src: srcFiles
+			}
+		}
+	});
+};
