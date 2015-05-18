5430ec4e20cdbd42b62972083160fb9140211eb9
Kevin Lewis <kevinlewis13@gmail.com>
2015-05-18 10:49:59 -0700

    creates eventSchema

---
+++ models/Event.js
@@ -0,0 +1,75 @@
+'use strict';
+
+var mongoose = require('mongoose');
+
+var guessSchema = mongoose.Schema({
+  title: String,
+  yes: {
+    userIds: [String],
+    total: Number
+  },
+  no: {
+    userIds: [String],
+    total: Number
+  }
+});
+
+var eventSchema = mongoose.Schema({
+  eventId: String,
+  eventName: String,
+  eventTime; Date,
+  bets: [guessSchema]
+});
+
+module.exports = mongoose.model('Event', eventSchema);
+
+    // first: {
+    //   yes: {
+    //     userIds: [String],
+    //     total: Number
+    //   },
+    //   no: {
+    //     userIds: [String],
+    //     total: Number
+    //   }
+    // },
+    // second: {
+    //   yes: {
+    //     userIds: [String],
+    //     total: Number
+    //   },
+    //   no: {
+    //     userIds: [String],
+    //     total: Number
+    //   }
+    // },
+    // third: {
+    //   yes: {
+    //     userIds: [String],
+    //     total: Number
+    //   },
+    //   no: {
+    //     userIds: [String],
+    //     total: Number
+    //   }
+    // },
+    // fourth: {
+    //   yes: {
+    //     userIds: [String],
+    //     total: Number
+    //   },
+    //   no: {
+    //     userIds: [String],
+    //     total: Number
+    //   }
+    // },
+    // fifth: {
+    //   yes: {
+    //     userIds: [String],
+    //     total: Number
+    //   },
+    //   no: {
+    //     userIds: [String],
+    //     total: Number
+    //   }
+    // }
