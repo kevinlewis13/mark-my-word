633c061e07ba49c639d6161a4537e0b57280788f
Stefan Redfield <redfield.stefan@gmail.com>
2015-05-20 16:54:18 -0700

    update names to home and away

--- models/Event.js
+++ models/Event.js
@@ -4,7 +4,8 @@ var mongoose = require('mongoose');
 
 var eventSchema = new mongoose.Schema({
   eventId: String,
-  eventName: String,
+  home: String,
+  away: String,
   eventTimeUnix: Number,
   eventTimeString:String,
   questions:[
