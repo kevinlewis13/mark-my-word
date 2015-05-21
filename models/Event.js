3f1dd63a06d6bb00038316dfbd80c4abbee3be15
Stefan Redfield <redfield.stefan@gmail.com>
2015-05-21 15:32:38 -0700

    update findUsers method

--- models/Event.js
+++ models/Event.js
@@ -2,6 +2,8 @@
 
 var mongoose = require('mongoose');
 var Vote = require('./Vote');
+var EventEmitter = require('events').EventEmitter;
+var ee = new EventEmitter();
 
 var eventSchema = new mongoose.Schema({
   eventId: String,
@@ -16,19 +18,33 @@ var eventSchema = new mongoose.Schema({
     }
   ],
   users: []
-
 });
 
 eventSchema.methods.findUsers = function() {
-  Vote.find({eventId : this.eventId}, function(err, votes) {  
+  var that = this;
+  Vote.find({eventId: this._id}, function(err, votes) {  
     votes.forEach(function(vote) {
-      if(this.users.indexOf(vote.userId) === -1) {
-        this.users.push(vote.userId)
+      if(that.users.indexOf(vote.userId) === -1) {
+        that.users.push(vote.userId);
+        that.save(function(err){
+          if (err) {console.log(err);}
+        });
       }
+      ee.emit('usersDone');
     });
   });
-}
+};
 
 module.exports = mongoose.model('Event', eventSchema);
 
- 
+// eventSchema.methods.findUsers = function(array) {
+//   Vote.find({eventId: this._id}, function(err, votes) {  
+//     votes.forEach(function(vote) {
+//       if(array.indexOf(vote.userId) === -1) {
+//         array.push(vote.userId);
+//         this.users.push(vote.userId);
+//         this.save;
+//       }
+//     }).bind(this);
+//   });
+// };
